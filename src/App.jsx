daa3c2dff5db2ea6adde366a01fb9e68d1b5417d
AbbasAkbar1221 <abbasakbar765@gmail.com>
2025-01-15 11:33:28 +0530

    carousel and sidemenu

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Header from './components/header/Header';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
-import Bottom from './components/footer/Bottom';
+import Carousel from './components/carousel/Carousel';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Navbar/>
+      <Carousel/>
       <Footer/>
     </div>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { useState } from "react";
+import SideMenu from "../slidemenu/SlideMenu";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <>
       <div className="bg-gray-800 text-white px-4 py-2 text-sm flex space-x-6">
-        <span className="material-icons text-white -mr-5 cursor-pointer">
+        <span
+          className="material-icons text-white -mr-5 cursor-pointer"
+          onClick={toggleMenu}
+        >
           menu
         </span>
         <span className="cursor-pointer hover:underline">All</span>
@@ -20,6 +30,7 @@ const Navbar = () => {
         <span className="cursor-pointer hover:underline">Home & Kitchen</span>
         <span className="cursor-pointer hover:underline">Amazon Pay</span>
       </div>
+      <SideMenu isOpen={isMenuOpen} toggleMenu={toggleMenu} />
     </>
   );
 };
