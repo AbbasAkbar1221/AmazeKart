1991f67a492608fb7f6382a0069cdec254abeaa5
AbbasAkbar1221 <abbasakbar765@gmail.com>
2025-01-22 10:55:40 +0530

    update account page

--- src/components/header/AccountAndList.jsx
+++ src/components/header/AccountAndList.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const AccountAndList = () => {
+  const currentUser = useSelector((state) => state.auth.currentUser);
   return (
     <Link to= "/register">
     <div className="hidden md:flex flex-col text-sm cursor-pointer border border-transparent hover:border-white">
-      <p className="font-semibold">Hello, sign in</p>
+      <p className="font-semibold">{currentUser ? `Hello, ${currentUser.username}` : "Hello, sign in"}</p>
       <p className="font-bold">Account & Lists</p>
     </div>
     </Link>
