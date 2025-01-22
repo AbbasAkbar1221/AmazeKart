df0c2f8e2e56879f2eeab5c0640b196497e65737
AbbasAkbar1221 <abbasakbar765@gmail.com>
2025-01-23 01:06:04 +0530

    update header

--- src/components/header/Location.jsx
+++ src/components/header/Location.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Location = () => {
+  const currentUser = useSelector(state => state.auth.currentUser)
   return (
     <div className="hidden md:flex items-center space-x-1 border border-transparent hover:border-white">
       <span className="material-icons">location_on</span>
       <div>
-        <p className="text-sm text-gray-500">Delivering to Delhi</p>
+        <p className="text-xs text-gray-300">{currentUser ? `Deliver to ${currentUser.username}` : "Deliver to Customer"}</p>
         <p className="text-sm font-semibold">Update location</p>
       </div>
     </div>
